cfd5bc7a8b7660526a30d32a52d6543d30fcb191
AjjlalAhmed <ajjlalahmed48@gmail.com>
2022-01-09 21:47:41 +0500

    Uploading code

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>Qr scanner prototype</h1>
+  <video ref="stream"></video>
+</template>
+
+<script>
+import QrScanner from "qr-scanner";
+import { ref } from "@vue/reactivity";
+import { onMounted } from "@vue/runtime-core";
+export default {
+  name: "App",
+  setup() {
+    const stream = ref(null);
+    onMounted(() => {
+      const qrScanner = new QrScanner(stream.value, (result) =>
+        console.log("decoded qr code:", result)
+      );
+      qrScanner.start();
+    });
+    return { stream };
+  },
+};
+</script>
+
+<style>
+h1 {
+  font-size: 2em;
+  text-align: center;
+  padding: 20px;
+  font-family: monospace;
+}
+</style>
