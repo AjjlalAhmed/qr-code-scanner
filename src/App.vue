4f27be51889f076ebae477df68e12ccefc9d1089
AjjlalAhmed <ajjlalahmed48@gmail.com>
2022-01-10 16:08:54 +0500

    uploading code

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,21 @@ export default {
     const startScanning = () => {
       if (!hasCamera.value)
         return alert("This device does not have any camera");
-      scanner.value.start();
+      scanner.start().then(() => {
+        
+        // List cameras after the scanner started to avoid listCamera's stream and the scanner's stream being requested
+        // at the same time which can result in listCamera's unconstrained stream also being offered to the scanner.
+        // Note that we can also start the scanner after listCameras, we just have it this way around in the demo to
+        // start the scanner earlier.
+        QrScanner.listCameras(true).then((cameras) =>
+          cameras.forEach((camera) => {
+            const option = document.createElement("option");
+            option.value = camera.id;
+            option.text = camera.label;
+            camList.add(option);
+          })
+        );
+      });
     };
 
     return { stream, startScanning, qrResult, qrError };
