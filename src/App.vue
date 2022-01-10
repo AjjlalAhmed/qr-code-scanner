42f7158a7773901d4476c562b81050e306428956
AjjlalAhmed <ajjlalahmed48@gmail.com>
2022-01-10 19:23:44 +0500

    uploading code

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- <h1>Qr scanner prototype</h1>
+  <h1>Qr scanner prototype</h1>
   <div class="container">
     <video class="stream" ref="stream"></video>
   </div>
@@ -13,96 +13,38 @@
   <div class="result">
     <h1>Error</h1>
     <p>{{ qrError }}</p>
-  </div> -->
-  <div class="jsqr">
-    <h1>Qr scanner prototype</h1>
-    <video width="300" height="300" class="video"></video>
-    <div id="output" v-if="showCanvas">
-      <div v-if="!outputData">No QR code detected.</div>
-      <div v-else>
-        <b>Data:</b>
-        <span id="outputData">{{ outputData }}</span>
-      </div>
-    </div>
-    <button @click="openScan">start</button>
   </div>
 </template>
 
 <script>
-// import QrScanner from "./assets/js/qr-scanner";
-// QrScanner.WORKER_PATH = "./assets/js/qr-scanner.worker";
+import QrScanner from "./assets/js/qr-scanner.min.js";
+QrScanner.WORKER_PATH = "./assets/js/qr-scanner-worker.min.js";
 import { ref } from "@vue/reactivity";
 import { onMounted } from "@vue/runtime-core";
-import adapter from "webrtc-adapter";
-import jsQR from "jsqr";
 export default {
   name: "App",
   setup() {
-    const video = ref(null);
-    const showCanvas = ref(false);
-    const outputData = ref(undefined);
-    const canvas = ref(null);
-    const ctx = ref(null);
-
-    onMounted(() => {
-      video.value = document.querySelector(".video");
-      canvas.value = document.createElement("canvas");
-      ctx.value = canvas.value.getContext("2d");
+    const stream = ref(null);
+    const qrResult = ref(null);
+    const qrError = ref(null);
+    const scanner = ref(null);
+    const hasCamera = ref(false);
+    onMounted(async () => {
+      hasCamera.value = await QrScanner.hasCamera();
+      if (!hasCamera.value) return (qrError.value = "Camera not found");
+      const videoTag = document.querySelector(".stream");
+      scanner.value = new QrScanner(
+        videoTag,
+        (result) => (qrResult.value = result),
+        (error) => (qrError.value = error)
+      );
     });
-
-    const tick = (data) => {
-      ctx.value.clearRect(0, 0, 400, 400);
-      ctx.value.drawImage(video.value, 0, 0, 400, 400);
-      var data = ctx.getImageData(0, 0, 400, 400).data;
-      const code = jsQR(data, width, height);
-
-      if (code) {
-        console.log("Found QR code", code);
-      }
+    const startScanning = async () => {
+      if (!hasCamera.value)
+        return alert("This device does not have any camera");
+      await scanner.value.start();
     };
-
-    const openScan = () => {
-      navigator.mediaDevices
-        .getUserMedia({
-          audio: false,
-          video: {
-            facingMode: "environment",
-          },
-        })
-        .then((stream) => {
-          video.value.srcObject = stream;
-          requestAnimationFrame(tick);
-        })
-        .catch(console.error);
-    };
-
-    return {
-      openScan,
-      showCanvas,
-      outputData,
-    };
-
-    // const stream = ref(null);
-    // const qrResult = ref(null);
-    // const qrError = ref(null);
-    // const scanner = ref(null);
-    // const hasCamera = ref(false);
-    // onMounted(async () => {
-    //   hasCamera.value = await QrScanner.hasCamera();
-    //   if (!hasCamera.value) return (qrError.value = "Camera not found");
-    //   const videoTag = document.querySelector(".stream")
-    //   scanner.value = new QrScanner(
-    //     videoTag,
-    //     (result) => (qrResult.value = result),
-    //     (error) => (qrError.value = error)
-    //   );
-    // });
-    // const startScanning = async () => {
-    //   if (!hasCamera.value)
-    //     return alert("This device does not have any camera");
-    //   await scanner.value.start();
-    // };
-    // return { stream, startScanning, qrResult, qrError };
+    return { stream, startScanning, qrResult, qrError };
   },
 };
 </script>
